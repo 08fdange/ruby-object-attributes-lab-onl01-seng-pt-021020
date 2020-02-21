4a48325b3bc9d49b1b1d37941cedf4d7f9334ffe
Frank  D'Angelo <08fdange@gmail.com>
2020-02-21 22:56:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,11 +2,11 @@ class Dog
   def initialize(dog_name)
     @dog_name = dog_name
   end
-  def name=(dog_name)
-    @dog_name = dog_name
+  def name=(name)
+    @name = name
   end
   
   def name
-    @dog_name
+    @name
   end
 end
